0718db6e309e3c7f8dc153e65fca4193d205bef8
Mohamed Kamal <mohamed.darwish@cegedim.com>
2020-05-15 20:18:15 +0200

    fix ui bugs

--- lari_site/static/js/main.js
+++ lari_site/static/js/main.js
@@ -53,4 +53,17 @@ function myMouseOut() {
   }, function () {
     $(this).children(".sub-menu").removeClass("submenu-opened");
   });
+
+  // Add minus icon for collapse element which is open by default
+  $(".collapse.show").each(function(){
+    $(this).prev(".card_pagee").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+  });
+  
+  // Toggle plus minus icon on show hide of collapse element
+  $(".collapse").on('show.bs.collapse', function(){
+    $(this).prev(".card_pagee").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+  }).on('hide.bs.collapse', function(){
+    $(this).prev(".card_pagee").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+  });
+
 });
